31b5fe8a61c40a7ba695bed5383268b46a75d09a
moha_amini <mhaminii18@gmail.com>
2024-08-23 15:08:06 +0330

    table of category and create new category controller and and view added.

---
+++ BulkyMVC/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyMVC.Data;
+using BulkyMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category) {
+            //if (category.Name == category.DisplayOrder.ToString()) {
+            //    ModelState.AddModelError("name", "the Display cannot be same as Name");
+            //}
+
+            //if (category.Name != null && category.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "test is invalid name");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                 return RedirectToAction("Index","Category");
+            }
+            return View();
+        }
+    }
+}
