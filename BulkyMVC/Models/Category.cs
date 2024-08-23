7631a53f58591b0b8eac095310988f170941248d
moha_amini <mhaminii18@gmail.com>
2024-08-23 15:06:31 +0330

    add category model with validation

--- BulkyMVC/Models/Category.cs
+++ BulkyMVC/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyMVC.Models
 {
@@ -7,7 +8,11 @@ namespace BulkyMVC.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Oder")]
+        [Range(1,100, ErrorMessage ="Dispaly order must Be between 1-100")]
         public int DisplayOrder { get; set; }
     
     }
